ab4ee24fba284b425c67e20017b5c82bae9b3bf8
nisha-manchandia <nishamanchandia0225@gmail.com>
2026-08-27 22:37:16 +0530

    Fix Alembic database configuration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -15,7 +15,10 @@ config = context.config
 
 database_url = os.getenv("DATABASE_URL")
 if database_url:
-    config.set_main_option("sqlalchemy.url", database_url)
+    config.set_main_option(
+        "sqlalchemy.url",
+        database_url.replace("%", "%%")
+    )
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
